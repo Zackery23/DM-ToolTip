224ca8cdd847ddf77d98545f1e529be224bfa142
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-01-31 17:40:52 -0500

    Update monster.js

--- js/monster.js
+++ js/monster.js
@@ -1,9 +1,14 @@
-$.getJSON('https://www.dnd5eapi.co/api/monsters/adult-black-dragon/', function(data){
-        var text = `Name: ${data.name}<br>
-                    Health: ${data.hit_points}<br>`
-        $(".mypanel").html(text);
-        for(var i = data.actions.length; i <= 0; i--) {
-            var text2 = `Actions:${data.actions[i].desc}<br>`
-            $(".actions").html(text2);
-        }
-});
+
+function monsterSearch(){
+    $.getJSON('https://www.dnd5eapi.co/api/monsters/', function(data){
+            var searchField = "name";
+            var searchVal = "";
+            for (var i = 0; i <= data.results.length; i++){
+                var str = data.results[i][searchField]
+                if ( str.charAt(0) == searchVal){
+                    console.log(data.results[i][searchField]);
+                }
+            }
+            
+    });
+}
