6e1c3f59562538d9917a8d6f562203153b21c166
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-01-31 17:53:50 -0500

    Update monster.js

--- js/monster.js
+++ js/monster.js
@@ -1,14 +1,19 @@
 
 function monsterSearch(){
+    var e = document.getElementById("monsterSelect");
+    var table = document.createElement("table");
+    var row = document.createElement("row");
+    document.getElementById("submit1").appendChild(table);
+    console.log(e.value);
     $.getJSON('https://www.dnd5eapi.co/api/monsters/', function(data){
             var searchField = "name";
-            var searchVal = "";
+            var searchVal = e.value;
             for (var i = 0; i <= data.results.length; i++){
                 var str = data.results[i][searchField]
                 if ( str.charAt(0) == searchVal){
                     console.log(data.results[i][searchField]);
                 }
+                
             }
-            
     });
 }
