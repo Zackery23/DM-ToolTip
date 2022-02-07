6217e3d30ea04309348758bca1cae3bc04ec881d
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-02-07 17:56:18 -0500

    Update monster.js

--- js/monster.js
+++ js/monster.js
@@ -2,44 +2,55 @@
 function monsterSearch(){
     if (document.getElementById('monsterInput').innerHTML == ''){
         createTable();
+
+        var mTable = document.getElementById("monsterTable");
+        var header = mTable.createTHead();
+        var row = header.insertRow();
+
+        var hCell1 = row.insertCell();
+        hCell1.innerHTML = "Name";
+
+        var hCell2 = row.insertCell();
+        hCell2 = row.innerHTML = "HitPoints";
     }
     else{
-        let myTable = document.getElementById("monsterTable");
-        myTable.innerHTML = "";
+        document.getElementById("monsterTable").remove();
+        
     }
-    var mTable = document.getElementById("monsterTable");
 
-    var e = document.getElementById("monsterSelect");
+    var mTable = document.getElementById("monsterTable");
+    var mBody  = document.getElementById("mBody")
+    var e = document.getElementById("searchMonster");
+
     console.log(e.value);
     $.getJSON('https://www.dnd5eapi.co/api/monsters/', function(data){
             var searchField = "name";
             var searchVal = e.value;
-            
+
             for (var i = 0; i <= data.results.length; i++){
                 var str = data.results[i][searchField]
-                if ( str.charAt(0) == searchVal){
+                if ( str.includes(searchVal)){
                     console.log(data.results[i][searchField]);
-                    var row = mTable.insertRow();
-                    var cell1 = row.insertCell(0);
-                    cell1.innerHTML = data.results[i][searchField];
+                    row = mBody.insertRow();
+
+                    var cell = row.insertCell();
+                    cell.innerHTML = data.results[i][searchField];
+                    
+                    var cell2 = row.insertCell()
+                    cell2.innerHTML = "test";
+
                 }
                 
             }
     });
 }
-function deleteRows(){
-    var new_tbody = document.createElement('tbody');
-    populate_with_new_rows(new_tbody);
-    old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
-
-}
 function createTable(){
     let table = document.createElement('table');
     table.setAttribute("id", "monsterTable" );
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
     
-    table.appendChild(thead);
+    let tbody = document.createElement('tbody');
+    tbody.setAttribute("id", "mBody");
+    
     table.appendChild(tbody);
     
     // Adding the entire table to the div tag
