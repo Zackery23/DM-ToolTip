b376753d0bcbfc938617a9a8cb21e9eae569ff8b
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-02-18 01:12:52 -0500

    Update monster.js

--- js/monster.js
+++ js/monster.js
@@ -1,58 +1,35 @@
+const searchValue = document.getElementById("searchMonster");
 
-function monsterSearch(){
-    if (document.getElementById('monsterInput').innerHTML == ''){
-        createTable();
+async function loadMonsterTable(url, table, searchValue){
+    const tableBody = table.querySelector("tbody");
 
-        var mTable = document.getElementById("monsterTable");
-        var header = mTable.createTHead();
-        var row = header.insertRow();
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    //clear table body
+    tableBody.innerHTML = "";
 
-        var hCell1 = row.insertCell();
-        hCell1.innerHTML = "Name";
-    }
-    else{
-        document.getElementById("monsterTable").remove();
+    // add monsters that fit the search value
+    for (let i = 0; i <= data.monsters.length; i++){
+        let str = data.monsters[i]["name"];
+
+        if (str.includes(searchValue)){
+            const rowElement = document.createElement("tr");
+
+            var cellElement1 = rowElement.appendChild(document.createElement("td"));
+            var cellElement2 = rowElement.appendChild(document.createElement("td"));
+            var cellElement3 = rowElement.appendChild(document.createElement("td"));
+            var cellElement4 = rowElement.appendChild(document.createElement("td"));
+
+            cellElement1.innerHTML = data.monsters[i]["name"];
+            cellElement2.innerHTML = data.monsters[i]["hit_points"];
+            cellElement3.innerHTML = data.monsters[i]["challenge_rating"];
+            cellElement4.innerHTML = data.monsters[i].actions[0]["name"];
+            tableBody.appendChild(rowElement);
+        }
         
     }
-
-    var mTable = document.getElementById("monsterTable");
-    var mBody  = document.getElementById("mBody")
-    var e = document.getElementById("searchMonster");
-
-    console.log(e.value);
-    $.getJSON('https://www.dnd5eapi.co/api/monsters/', function(data){
-            var searchField = "name";
-            var searchVal = e.value;
-            var health = "hit_points";
-
-            for (var i = 0; i <= data.results.length; i++){
-                var str = data.results[i][searchField]
-                if ( str.includes(searchVal)){
-                    console.log(data.results[i][searchField]);
-                    row = mBody.insertRow();
-
-                    var cell = row.insertCell();
-                    cell.innerHTML = data.results[i][searchField];
-                    
-                    $.getJSON('https://www.dnd5eapi.co/api/monsters/', function(data){
-                        
-                    });
-
-                }
-                
-            }
-    });
 }
-function createTable(){
-    let table = document.createElement('table');
-    table.setAttribute("id", "monsterTable" );
-    
-    let tbody = document.createElement('tbody');
-    tbody.setAttribute("id", "mBody");
-    
-    table.appendChild(tbody);
-    
-    // Adding the entire table to the div tag
-    document.getElementById('monsterInput').appendChild(table);
-
+function runSearch(){
+    loadMonsterTable("./monsters.json", document.querySelector("table"), searchValue.value)
 }
