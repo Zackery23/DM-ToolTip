fa3bdbc28adc7551eeacb80c6f0e3cabd3e7c5e8
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-02-21 00:38:29 -0500

    Update monster.js

--- js/monster.js
+++ js/monster.js
@@ -5,31 +5,60 @@ async function loadMonsterTable(url, table, searchValue){
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
+    console.log(searchValue);
     //clear table body
     tableBody.innerHTML = "";
 
-    // add monsters that fit the search value
-    for (let i = 0; i <= data.monsters.length; i++){
-        let str = data.monsters[i]["name"];
+    // change first charecter to upper case and add it back to the string
+    let sliceUp = searchValue.slice(1);
+    console.log(sliceUp);
+    let searchValueUp = searchValue.charAt(0).toUpperCase() + sliceUp;
+    console.log(searchValueUp);
 
-        if (str.includes(searchValue)){
-            const rowElement = document.createElement("tr");
+        // add monsters that fit the search value
+        for (let i = 0; i <= data.monsters.length; i++){
+            let str = data.monsters[i]["name"];
 
-            var cellElement1 = rowElement.appendChild(document.createElement("td"));
-            var cellElement2 = rowElement.appendChild(document.createElement("td"));
-            var cellElement3 = rowElement.appendChild(document.createElement("td"));
-            var cellElement4 = rowElement.appendChild(document.createElement("td"));
+            if (str.includes(searchValueUp)){
+                const rowElement = document.createElement("tr");
 
-            cellElement1.innerHTML = data.monsters[i]["name"];
-            cellElement2.innerHTML = data.monsters[i]["hit_points"];
-            cellElement3.innerHTML = data.monsters[i]["challenge_rating"];
-            cellElement4.innerHTML = data.monsters[i].actions[0]["name"];
-            tableBody.appendChild(rowElement);
-        }
-        
+                var cellElement1 = rowElement.appendChild(document.createElement("td"));
+                var cellElement2 = rowElement.appendChild(document.createElement("td"));
+                var cellElement3 = rowElement.appendChild(document.createElement("td"));
+                var cellElement4 = rowElement.appendChild(document.createElement("td"));
+
+                cellElement1.innerHTML = data.monsters[i]["name"];
+                cellElement2.innerHTML = data.monsters[i]["hit_points"];
+                cellElement3.innerHTML = data.monsters[i]["challenge_rating"];
+                cellElement4.innerHTML = data.monsters[i].actions[0]["name"];
+                tableBody.appendChild(rowElement);
+            }   
     }
 }
 function runSearch(){
     loadMonsterTable("./monsters.json", document.querySelector("table"), searchValue.value)
 }
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
+}
+  
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+}
+
+// Changes between the navigation oif the website
+function changeSection(evt, section) {
+  var i, navMove, navMovLink;
+  navMove = document.getElementsByClassName("navMove");
+  for (i = 0; i < navMove.length; i++) {
+    navMove[i].style.display = "none";
+  }
+  navMovLink = document.getElementsByClassName("navMovLink");
+  for (i = 0; i < navMovLink.length; i++) {
+    navMovLink[i].className = navMovLink[i].className.replace(" active", "");
+  }
+  document.getElementById(section).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+  
