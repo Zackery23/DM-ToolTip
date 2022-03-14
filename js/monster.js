1906e234f4cd3356fb2ee511a446af7ffd07e0c4
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-03-14 17:47:50 -0400

    Update monster.js

--- js/monster.js
+++ js/monster.js
@@ -1,5 +1,7 @@
 const searchValue = document.getElementById("searchMonster");
 
+
+
 async function loadMonsterTable(url, table, searchValue){
     const tableBody = table.querySelector("tbody");
 
@@ -39,34 +41,35 @@ async function loadMonsterTable(url, table, searchValue){
 function runSearch(){
     loadMonsterTable("data/monsters.json", document.querySelector("table"), searchValue.value)
 }
-function openNav() {
-  document.getElementById("mySideNav").style.width = "250px";
-}
+
+async function findIndexMonster(){
+  const response = await fetch("data/monsters.json");
+  const data = await response.json();
+  const makeMonsterVal = document.getElementById("makeMonster");
+
+
+  let mon = makeMonsterVal.value;
   
-function closeNav() {
-  document.getElementById("mySideNav").style.width = "0";
+
+  //console.log(data.monsters[makeMonsterVal]);
+  let sliceUp = mon.slice(1);
+  //console.log(sliceUp);
+  let searchValueUp = mon.charAt(0).toUpperCase() + sliceUp;
+
+  var index = data.monsters.indexOf(searchValueUp);
+
+  makeMonster(index);
+  
 }
 
-// Changes between the navigation oif the website
-function changeSection(evt, section) {
-  var i, navMove, navMovLink;
-  navMove = document.getElementsByClassName("navMove");
-  for (i = 0; i < navMove.length; i++) {
-    navMove[i].style.display = "none";
-  }
-  navMovLink = document.getElementsByClassName("navMovLink");
-  for (i = 0; i < navMovLink.length; i++) {
-    navMovLink[i].className = navMovLink[i].className.replace(" active", "");
-  }
-  document.getElementById(section).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+
 // Grabing info on a specific monster 
-async function makeMonster(){
+async function makeMonster(index){
+  console.log(index);
   // get the JSon data fro each monster loaded to be displayed when searched for creation
   const response = await fetch("data/monsters.json");
   const data = await response.json();
-  //console.log(data.monsters[10]);
+
 
   // grabing the ids for where the data shall be place into
   let monsterName = document.getElementById("monsterName")
@@ -80,60 +83,60 @@ async function makeMonster(){
   
 
   //adding monsters name, size ,type and alignment  to the div of what was searched
-  dataP1.innerHTML = data.monsters[10]["name"] + "<br />" 
-      + " Size: " + data.monsters[10]["size"] + " Type: " + data.monsters[10]["type"] 
-      + " Alignment: " + data.monsters[10]["alignment"];
+  dataP1.innerHTML = data.monsters[index]["name"] + "<br />" 
+      + " Size: " + data.monsters[index]["size"] + " Type: " + data.monsters[index]["type"] 
+      + " Alignment: " + data.monsters[index]["alignment"];
   monsterName.appendChild(dataP1);
   //console.log(monsterName);
  /* for (const key1 in data.monsters[10]) {
       console.log(data.monsters[10][key1])
   }
   */
-  dataP2.innerHTML = "Armor Class: " + data.monsters[10]["armor_class"] + "<br />" 
-      + " Health: " + data.monsters[10]["hit_points"] + 
-      + " (" + data.monsters[10]["hit_dice"] + ") " + "<br />" 
-      + " Speed: " + data.monsters[10]["speed"] + "<br />" 
+  dataP2.innerHTML = "Armor Class: " + data.monsters[index]["armor_class"] + "<br />" 
+      + " Health: " + data.monsters[index]["hit_points"] + 
+      + " (" + data.monsters[index]["hit_dice"] + ") " + "<br />" 
+      + " Speed: " + data.monsters[index]["speed"] + "<br />" 
       + " STR DEX CON INT WIS CHA" + "<br />" + "<br />" 
-      + data.monsters[10]["strength"] + " "
-      + data.monsters[10]["dexterity"] + " "
-      + data.monsters[10]["constitution"] + " "
-      + data.monsters[10]["intelligence"] + " "
-      + data.monsters[10]["wisdom"] + " "
-      + data.monsters[10]["charisma"] + " " + "<br />"
+      + data.monsters[index]["strength"] + " "
+      + data.monsters[index]["dexterity"] + " "
+      + data.monsters[index]["constitution"] + " "
+      + data.monsters[index]["intelligence"] + " "
+      + data.monsters[index]["wisdom"] + " "
+      + data.monsters[index]["charisma"] + " " + "<br />"
       + " Saving Throws: " 
       + " STR + " 
-      +  data.monsters[10]["strength_save"]
+      +  data.monsters[index]["strength_save"]
       + ", DEX  + "
-      +  data.monsters[10]["dexterity_save"]
+      +  data.monsters[index]["dexterity_save"]
       + ", CON + "
-      +  data.monsters[10]["constitution_save"]
+      +  data.monsters[index]["constitution_save"]
       + ", INT + "
-      +  data.monsters[10]["intelligence_save"]
+      +  data.monsters[index]["intelligence_save"]
       + ", Wis + "
-      +  data.monsters[10]["wisdom_save"]
+      +  data.monsters[index]["wisdom_save"]
       + ", CHA  + "
-      +  data.monsters[10]["charisma_save"] + "<br />" 
+      +  data.monsters[index]["charisma_save"] + "<br />" 
       + "Skills: " + "<br />" 
-      + "Damage Vulnerabilities " + data.monsters[10]["damage_vulnerabilities"] + "<br />" 
-      + "Damage Resistances: " + data.monsters[10]["damage_resistances"] + "<br />" 
-      + "Damage Immunities: " + data.monsters[10]["damage_immunities"] + "<br />" 
-      + "Condtion_immunities: " + data.monsters[10]["condition_immunities"] + "<br />"
-      + "Senses: " + data.monsters[10]["senses"] + "<br />"
-      + "Languages: " + data.monsters[10]["languages"] + "<br />"
-      + "Challenge Rating: " + data.monsters[10]["challenge_rating"];
+      + "Damage Vulnerabilities " + data.monsters[index]["damage_vulnerabilities"] + "<br />" 
+      + "Damage Resistances: " + data.monsters[index]["damage_resistances"] + "<br />" 
+      + "Damage Immunities: " + data.monsters[index]["damage_immunities"] + "<br />" 
+      + "Condtion_immunities: " + data.monsters[index]["condition_immunities"] + "<br />"
+      + "Senses: " + data.monsters[index]["senses"] + "<br />"
+      + "Languages: " + data.monsters[index]["languages"] + "<br />"
+      + "Challenge Rating: " + data.monsters[index]["challenge_rating"];
   
       monsterStats_Immunites.appendChild(dataP2);
       //loop trhough all actions and if the creature has lair actions
-      for (var key in data.monsters[10]["actions"]){
-        for (var key1 in data.monsters[10]["actions"][key]){
+      for (var key in data.monsters[index]["actions"]){
+        for (var key1 in data.monsters[index]["actions"][key]){
           let dataP3 = document.createElement("div");
-          let pulled = data.monsters[10]["actions"][key][key1];
+          let pulled = data.monsters[index]["actions"][key][key1];
           // check if the keys are the information that we want from the api          
           if (key1 == "name" || key1 == "desc"){
             // check for if the key1 is a lair action 
-            if(data.monsters[10]["actions"][key]["name"] == "Lair Actions"){
+            if(data.monsters[index]["actions"][key]["name"] == "Lair Actions"){
 
-              console.log(data.monsters[10]["actions"][key][key1]);
+              console.log(data.monsters[index]["actions"][key][key1]);
               // we need to alter \n to <br />
               var str = pulled;
               str = str.replace(/(?:\r\n|\r|\n)/g, '<br>');
@@ -153,14 +156,35 @@ async function makeMonster(){
 
       }
       // implements through if the the creatue has legendary actions
-      for (var key in data.monsters[10]["legendary_actions"]){
-        for (var key1 in data.monsters[10]["legendary_actions"][key]) {
+      for (var key in data.monsters[index]["legendary_actions"]){
+        for (var key1 in data.monsters[index]["legendary_actions"][key]) {
           if (key1 == "name" || key1 == "desc"){
             let dataP4 = document.createElement("div");
-            dataP4.innerHTML = data.monsters[10]["legendary_actions"][key][key1] + "<br />"
+            dataP4.innerHTML = data.monsters[index]["legendary_actions"][key][key1] + "<br />"
             actions.appendChild(dataP4);
           }
         }
       }
 }
-  
+// open nav menu
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
+}
+// close nav menu
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+}
+// Changes between the navigation oif the website
+function changeSection(evt, section) {
+  var i, navMove, navMovLink;
+  navMove = document.getElementsByClassName("navMove");
+  for (i = 0; i < navMove.length; i++) {
+    navMove[i].style.display = "none";
+  }
+  navMovLink = document.getElementsByClassName("navMovLink");
+  for (i = 0; i < navMovLink.length; i++) {
+    navMovLink[i].className = navMovLink[i].className.replace(" active", "");
+  }
+  document.getElementById(section).style.display = "block";
+  evt.currentTarget.className += " active";
+}
