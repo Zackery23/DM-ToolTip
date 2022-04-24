33884a3f19f6510115fe55aad7ee3017e5d2d110
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2022-04-23 23:09:42 -0400

    Update initiativeTracker.js

--- js/initiativeTracker.js
+++ js/initiativeTracker.js
@@ -1,1 +1,31 @@
-  
+const encounterRound = 1;
+const combatArray = [];
+
+function track(monsterName, dexmod){
+    let rolled = await rollD20();
+    let name = "";
+    let dexmod = 0;
+    //check for ties
+    for (i = 0; i >= combatArray.length; i++){   
+        if(rolled == combatArray[i].rolled){
+                
+        }
+    }
+
+
+    //check when round is at bottom
+    combatArray.push[
+        {
+           name: monsterName, 
+           dexmod: dexmod,
+           rolled: rolled
+        }
+    ];
+    console.log(combatArray);
+    //add to array 
+    encounterRound++;
+}
+async function rollD20(){
+    let die20 = Math.floor(Math.random() * 20) + 1;
+    return die20;
+}
